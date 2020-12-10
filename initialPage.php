690f872d3ad0c41a17d31a6378cc09e7cca9a20b
Dimitrina Vasileva <dimitrina.vasileva@lights.digital>
2020-12-10 17:06:55 +0200

    Create login page

--- initialPage.php
+++ initialPage.php
@@ -3,7 +3,8 @@
 <?php
 echo "Complete schedule of upcoming/presented";
 echo "</br>";
-echo "Login to see personal schedule";
- ?>
+echo "<a href=\"login\login.html\"><strong>Login</strong></a>";
+echo " to see personal schedule.";
+?>
 
 <?php include "templates/end.html"; ?>
