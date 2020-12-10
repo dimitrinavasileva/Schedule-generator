61ee39af2cee85a2b2759ed829fa05071b5654ab
Dimitrina Vasileva <dimitrina.vasileva@lights.digital>
2020-12-10 16:53:37 +0200

    Create initial page

---
+++ initialPage.php
@@ -0,0 +1,9 @@
+<?php include "templates/start.html"; ?>
+
+<?php
+echo "Complete schedule of upcoming/presented";
+echo "</br>";
+echo "Login to see personal schedule";
+ ?>
+
+<?php include "templates/end.html"; ?>
